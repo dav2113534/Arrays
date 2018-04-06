e4bb38c807e89e51a7403430f8eff9c759eed681
David Palacios <dpalacios03081991@gmail.com>
2018-04-06 06:05:05 -0700

    added arrays onto listbox1

--- Arrays/Form1.Designer.cs
+++ Arrays/Form1.Designer.cs
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(40, 86);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(94, 32);
+            this.btnClear.TabIndex = 0;
+            this.btnClear.Text = "Arrays One";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(191, 86);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(164, 184);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 522);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnClear);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- Arrays/Form1.cs
+++ Arrays/Form1.cs
@@ -16,5 +16,24 @@ namespace Arrays
         {
             InitializeComponent();
         }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            int[] lottery_numbers;
+            lottery_numbers = new int[4];
+
+            lottery_numbers[0] =1;
+            lottery_numbers[1] =2;
+            lottery_numbers[2] =3;
+            lottery_numbers[3] =4;
+
+            listBox1.Items.Add(lottery_numbers[0]);
+            listBox1.Items.Add(lottery_numbers[1]);
+            listBox1.Items.Add(lottery_numbers[2]);
+            listBox1.Items.Add(lottery_numbers[3]);
+
+        }
     }
 }
